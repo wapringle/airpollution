541da7de0ae7236f7767a3b936f71816d5be47cd
Bill <bllprngl@gmail.com>
2020-06-17 17:19:27 +0100

    final polish

--- bib.py
+++ bib.py
@@ -1,12 +1,11 @@
-from browser import document, html, window, alert, timer, ajax
+from browser import document, html, window, alert, timer
 from  browser.html import *
-import json,sys
 import bibconfig
 
 """
 Script to create a canvas, load a picture and create a number of hightlight areas that produce a popup on mouseover.
 
-The highlight areas are rectangles that invisibly overlay the image. When the mouse enters the highlight area
+The highlight areas are rectangles that invisibly overlay the image. When the mouse enters the highlight area and is clicked
 a popup appears over the area. The coordinates in he table below give the 
 top left and bottom right of the overlay, relative to the coordinates of the underlying image. These are
 converted to absolute coordinates by adding the XY coordinates of the image.
@@ -15,52 +14,16 @@ The mouse coordinates are displayed in the table for diagnostic purposes.
 
 """
  
-dbg=True
+dbg=False
+clickModel=True
+
 pageIndex=0
 jsonConfig=[]
-def read(f):
-    global pageIndex,jsonConfig
-    if False:
-        jsonConfig=json.loads(f.read())
-    else:
-        jsonConfig=bibconfig.config
-    print('loaded')
-    displayPic(document['action'],jsonConfig[pageIndex])
 
-def loadConfig():
-    print("loading")
-    req = ajax.get("config/bib.jsonld", mode="text", oncomplete=read)
-
-def fastConfig():
+def load():
+    """ Config is stored as python structure in bibconfig.py """
     global pageIndex,jsonConfig
-    jsonConfig.append(
-        {'filename': '1-BiB-Slide.png',
-         'highlights': [{'brx_pc': '9',
-                         'bry_pc': '33',
-                         'star': '1',
-                         'text': 'The atmosphere acts like a jacket to keep the planet '
-                                 'warm.  If there was no atmosphere the Earth would be '
-                                 'about -18 C. Brr',
-                         'tlx_pc': '2',
-                         'tly_pc': '20'},
-                        {'brx_pc': '48',
-                         'bry_pc': '85',
-                         'star': '2',
-                         'text': 'If the earth was the size on an apple, the '
-                                 'atmosphere would be thinner than the skin of the '
-                                 'apple.',
-                         'tlx_pc': '42',
-                         'tly_pc': '72'}],
-         'ratio': '2.6'}
-)
-    displayPic(document['action'],jsonConfig[pageIndex])
-    
-    
-def update_checkboxes(ev):
-    # updates checkboxes when the selection has changed
-    #selected = [option.value for option in sel if option.selected]
-    pass
-    
+    jsonConfig=bibconfig.config
 
 def frontPage():
     
@@ -81,7 +44,9 @@ def frontPage():
         main.clear()
         
         displayPic(main,jsonConfig[pageIndex])
-    
+
+    """ Elements are laid out using the grid css model. Could replace with a table """
+
     document <= DIV(
         DIV( Class="item6",id="Left2")+
         DIV( Class="item1",id="Header")+
@@ -102,28 +67,30 @@ def frontPage():
     r.bind("click",shift_right)
     main= DIV(id="action",style={"width": "100%", "height": "80%"})
     document["Main"] <= main
-    #document["Footer"] <= SPAN("FOOTER")
-    
-            
 
+    displayPic(main,jsonConfig[pageIndex])
+    
 
 class Bunch(object):
+    """ Save the lines from slides.cvs file as attributes. Easier to handle """
     def __init__(self, adict):
         self.__dict__.update(adict)
         self.width=int(window.innerWidth) - 20 # * 7 /8)
-        #print(f'width {self.width}')
         self.height=int(self.width/float(self.ratio))
         self.picture='img/'+self.filename
 
 def cv(a,b):
+    """ Convert percentage to absolute """
     a=0 if a=='' else int(a)
     return int(a*b/100 +0.5)
 
 def vc(a,b):
+    """ and vice versa """
     return int(a*100/b +0.5)
     
 
 class Highlight(object):
+    """ Ditto for Popup_Summary.csv, coverting percentages to absolute """
     def __init__(self, adict,q):
         
         self.__dict__.update(adict)
@@ -134,20 +101,21 @@ class Highlight(object):
         
 
 def displayPic(doc,config):
+    
+    """ Display apicure using data from config """
     def px(x):
         return str(x)+"px"
+    
     q=Bunch(config)
-    q.width=doc.offsetWidth-40
+    q.width=doc.offsetWidth-40 # make it as fullscreen as possible, allow for margins
     q.height=int(q.width/float(q.ratio))
     canvas = CANVAS(id="canvas",width =q.width, height = q.height)
-    #canvas.style={"position": "absolute", "left": px(q.startX), "top": px(q.startY) }    
     canvas.bind("mousemove", mousemove)
     
     doc <= canvas # this brython operator writes the canvas to the base document
 
     q.startX=canvas.offsetLeft
     q.startY=canvas.offsetTop
-    #print(f"canvas {q.startX} {q.startY} ")
     
     """ Cant work out how to load image into canvas with brython so drop into javascript here   """
 
@@ -167,16 +135,21 @@ def displayPic(doc,config):
 
         """ Bind the mouse events to the individual highlight areas """
         
-        if True:
+        if clickModel:
+            
+            """ Click over area for popup """
             c2.bind('mousedown', mouseenter)
             c2.bind('mouseup', mouseleave)
             c2.bind('mouseleave', mouseout)
         else:
+            
+            """ Else mouseover """
             c2.bind('mouseenter', mouseenter)
             c2.bind('mouseleave', mouseout)
 
         doc <= c2
 
+        """ This is the popup """
         
         popup_id="popup"+highlight_id
         txt=SPAN(h.text,id=popup_id, Class="tooltiptext")
@@ -184,14 +157,13 @@ def displayPic(doc,config):
         tooltip=DIV(txt )
         tooltip.style={
             "position": "absolute", 
-            "left": px(min((h.tlx+h.brx)/2+q.startX, q.width - 500 )), 
+            "left": px(min((h.tlx+h.brx)/2+q.startX, q.width - 500 )), #  if necessary, adjust lhs to stop popup overflowing page
             "top": px((h.tly+h.bry)/2+q.startY),
             "zIndex": 1,
             'visibility': 'hidden' 
         }
         
-        
-        if True:
+        if clickModel:
             txt.bind('mouseup', mouseleave)
             txt.bind('mouseleave', mouseout)
         else:
@@ -212,24 +184,20 @@ def displayPic(doc,config):
     
     return doc
     
-    
-    
-
 """ mouse events """
+
 def mousemove(ev):
     if dbg: document["trace3"].text = f"coordinates : {vc(ev.layerX,document['canvas'].offsetWidth)}, {vc(ev.layerY,document['canvas'].offsetHeight)}"
     pass
 
-
 def mouseenter(ev):
     if dbg: document["trace1"].text = f'entering {ev.currentTarget.id}'
     pp= document["popup"+ev.currentTarget.id]
-    print(f"tooltip {pp.id} width {pp.offsetWidth}")
     pp.style['visibility']='visible' # turn popup on 
 
 def mouseleave(ev):
+    
     if dbg: document["trace1"].text = f'leaving {ev.currentTarget.id}'
-    #document[ev.currentTarget.id].style['visibility']='hidden' # turn popup off
 
     id=ev.currentTarget.id
     if id[:5]=="popup":
@@ -245,7 +213,6 @@ def mouseleave(ev):
     document[popup_id].style['visibility']='hidden' # turn popup off
     
 def mouseout(ev):
-    global cvs
     id=ev.currentTarget.id
     if id[:5]=="popup":
         frame_id=id[5:] 
@@ -260,11 +227,14 @@ def mouseout(ev):
     X=ev.clientX
     Y=ev.clientY
 
-    #document["trace3"].text = f"coordinates : {X}, {Y}"
-    document["trace1"].text = f'leaving {ev.currentTarget.id}  {X}, {Y} { X - box.offsetLeft} {Y - box.offsetTop} '
+    if dbg: document["trace1"].text = f'leaving {ev.currentTarget.id}  {X}, {Y} { X - box.offsetLeft} {Y - box.offsetTop} '
+    
+    """ Ignore if mouse still inside popup or star """
+    
     if  0 <= X - box.offsetLeft  <= box.width \
     and 0 <= Y - box.offsetTop   <= box.height :    
         return
-    document[popup_id].style['visibility']='hidden' # turn popup off
     
-
+    """ else turn popup off """
+    document[popup_id].style['visibility']='hidden' 
+    
