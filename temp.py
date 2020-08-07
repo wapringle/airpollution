c0caef54a8c2e7996b5558dedfb3ff1896f5563f
wapringle <bllprngl@gmail.com>
2020-08-07 15:13:30 +0000

    images

--- temp.py
+++ temp.py
@@ -1,23 +1,20 @@
 from browser import document, html, window, alert, timer, ajax
-from wordsquare import *
+from wordsquare import on_mouse_enter,on_grid_button_pressed,load
+import wordsquare
 
 import pprint
-
-
-class x2():
-    def __init__(self,id):
-        self.id=id
-    pass
-class x1():
-    def __init__(self,id):
-        self.currentTarget=x2(id)
         
-def null():
-    pass
+load()
 
-frontPage=null
-showPuzzle()
-
-ev=x1("i0101")
+ev=html._EV("i0101")
+dd=html.DIV("hello",id="hello")
 on_mouse_enter(ev)
 on_grid_button_pressed(ev)
+on_grid_button_pressed(ev)
+global activeSquares
+for id in wordsquare.activeSquares:
+    ev=html._EV(id)
+    on_mouse_enter(ev)
+    on_grid_button_pressed(ev)
+    
+i=1
